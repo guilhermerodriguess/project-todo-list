8cb2e5d6d7fae5bfc962da069f8fe9db9071a9ec
Guilherme R <grsguiga@gmail.com>
2022-01-09 22:47:04 -0300

    Requisito 7, 8 e 9

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // No campo de input será digitado o texto de uma tarefa qualquer e, em seguida, clicar-se-á no botão de criar tarefa. Será verificado que, após o clique, o texto digitado aparece na lista e desaparece do input.
 
 var acessButton = document.getElementById('criar-tarefa')
+var acessOl = document.getElementById('lista-tarefas')
+
 
 acessButton.addEventListener('click', AdicionaTarefa)
 
@@ -12,4 +14,29 @@ function AdicionaTarefa () {
 
     acessOl.appendChild(createTask)
     acessInput.value = ''
+}
+
+acessOl.addEventListener('click', seleciona)
+
+function seleciona (origem) {
+  let acessLi = document.querySelectorAll('li')
+  origem.target.classList.add('select')
+
+  for(i = 0; i < acessLi.length; i++) {
+    if (acessLi[i].classList.contains('select') && acessLi[i].innerText !== origem.target.innerText) {
+      acessLi[i].classList.remove('select')
+    }
+  }
+}
+
+acessOl.addEventListener('dblclick', complete)
+
+function complete (origem) {
+
+  if (origem.target.classList.contains('completed')) {
+    origem.target.classList.remove('completed')
+  }
+  else {
+    origem.target.classList.add('completed')
+  }
 }
